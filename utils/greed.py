56a8812bf6c24316ee13de374da6d2f2b95d318e
Paulo Gonçalves Lima <paulogoncalves436@gmail.com>
2021-05-01 18:29:41 -0300

    Lista de tarefas escolhidas, mas com bug na hora de escolher as tarefas.

--- utils/greed.py
+++ utils/greed.py
@@ -2,11 +2,20 @@ from numpy.lib.function_base import select
 import trabalho as T
 import numpy as np
 
+class pahtJobs:
+    def __init__(self) -> None:
+        self.jobs = []
+        self.total = 0
+
+
 class greed:
     def __init__(self, ListaDeTarefas:list) -> None:
         self.listaDeTarefas = ListaDeTarefas
-        self.jobs = np.full((self.intervalParti(), len(ListaDeTarefas) + 1), T.trabalho(0,0,0), dtype="object")
-        self.jobs[:,-1] = 0
+        self.final = []
+
+        # linhas, colunas = (self.intervalParti(), len(ListaDeTarefas) + 1)
+        # self.jobs = [[T.trabalho(0,0,0)] * colunas] * linhas
+        
 
     def intervalParti(self) -> int:
         self.listaDeTarefas.sort(key=lambda x: x.comeco)
@@ -24,24 +33,12 @@ class greed:
                 
         return len(conjTarefas)
 
-
     def computeP(self, index) -> int:
         esq = 0
         dire = index - 1
-        testE = []
-        testD = []
-    
-
-        for i in self.listaDeTarefas:
-            testE.append(i.final)
-            testD.append(i.comeco)
-
-        
-        # print(f"{self.listaDeTarefas[index].nome} {index} - {esq + (dire - esq) // 2} {testD} {testE}")
-
 
         while esq <= dire:
-            meio = np.right_shift(dire + esq, 1)
+            meio = np.right_shift(dire + esq, 1) # Divisão por 2 fazendo bit sift para a direita 1x
 
             if self.listaDeTarefas[meio].final <= self.listaDeTarefas[index].comeco:
                 if self.listaDeTarefas[meio + 1].final <= self.listaDeTarefas[index].comeco:
@@ -55,25 +52,38 @@ class greed:
     
     def intervalSchWei(self):
 
-        for k in range(0, self.intervalParti()):
-            self.listaDeTarefas.sort(key=lambda x: x.final)
+        # for k in 
+        self.listaDeTarefas.sort(key=lambda x: x.final)
+        caminhosPossiveis = []
+        
+        for i in range(0, len(self.listaDeTarefas)):
+            caminhosPossiveis.append(pahtJobs())
+
+        caminhosPossiveis[0].total = self.listaDeTarefas[0].prioridade
+        caminhosPossiveis[0].jobs.append(self.listaDeTarefas[0])
+
+        for i in range(1, len(self.listaDeTarefas)):
+            tmpTotal = self.listaDeTarefas[i].prioridade
+            indexJob = self.computeP(i)
+
+            if indexJob != None:
+                tmpTotal += caminhosPossiveis[indexJob].total
             
-            self.jobs[k, 0] = self.listaDeTarefas[0]
-            self.jobs[k, -1] = self.listaDeTarefas[0].prioridade
+            if tmpTotal > caminhosPossiveis[i - 1].total and indexJob != None:
+                caminhosPossiveis[i].total = tmpTotal
 
-            for i in range(1, len(self.listaDeTarefas)):
+                caminhosPossiveis[i].jobs = caminhosPossiveis[indexJob].jobs
+                caminhosPossiveis[i].jobs.append(self.listaDeTarefas[i])
+            else:
+                caminhosPossiveis[i] = caminhosPossiveis[i - 1]
 
-                tmpJob = self.listaDeTarefas[i]
-                self.jobs[k, -1] = self.listaDeTarefas[i].prioridade
+            
+        for i in caminhosPossiveis[8].jobs:
+            print(i.nome)
+            self.listaDeTarefas.pop(self.listaDeTarefas.index(i))
+        print(self.listaDeTarefas)
 
-                P = self.computeP(i)
-                if P:
-                 self.jobs[k, -1] += self.jobs[k, P].prioridade
-
-
-
-
-g = greed([T.trabalho(0, 15, 15, "A"), T.trabalho(2, 16, 10,"B"),  T.trabalho(2, 17,10, "C"), T.trabalho(2, 18,10, "D"), T.trabalho(18, 20,10, "E"), T.trabalho(18, 20,10, "F"), T.trabalho(18, 20,27, "G"), T.trabalho(18, 20,10, "H"), T.trabalho(18, 20,10, "I")])
+g = greed([T.trabalho(0, 15, 15, "A"), T.trabalho(2, 16, 10,"B"),  T.trabalho(2, 17,10, "C"), T.trabalho(2, 18,10, "D"), T.trabalho(18, 20,10, "E"), T.trabalho(18, 20,10, "F"), T.trabalho(16, 20, 27, "G"), T.trabalho(18, 20,10, "H"), T.trabalho(18, 20,10, "I")])
 
 
 g.intervalSchWei()
