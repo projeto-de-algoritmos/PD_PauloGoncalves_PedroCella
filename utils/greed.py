e6a87dc29449f3d40236023d9ec94c55cee7f48d
Paulo Gonçalves Lima <paulogoncalves436@gmail.com>
2021-05-01 14:14:52 -0300

    Commit, grande: Funçao de interval particion, computeP e começo da funçao de interval Schedulind weighted.

--- utils/greed.py
+++ utils/greed.py
@@ -1,18 +1,19 @@
+from numpy.lib.function_base import select
 import trabalho as T
 import numpy as np
 
 class greed:
     def __init__(self, ListaDeTarefas:list) -> None:
         self.listaDeTarefas = ListaDeTarefas
-        self.jobs = np.full((self.intervalParti(ListaDeTarefas), len(ListaDeTarefas)), -1, dtype="int32")
-        
+        self.jobs = np.full((self.intervalParti(), len(ListaDeTarefas) + 1), T.trabalho(0,0,0), dtype="object")
+        self.jobs[:,-1] = 0
 
-    def intervalParti(self, ListaDeTarefas: list) -> int:
-        ListaDeTarefas.sort(key=lambda x: x.comeco)
+    def intervalParti(self) -> int:
+        self.listaDeTarefas.sort(key=lambda x: x.comeco)
 
-        conjTarefas = np.array([ListaDeTarefas[0].final], dtype="float")
+        conjTarefas = np.array([self.listaDeTarefas[0].final], dtype="float")
         
-        for currJob in ListaDeTarefas[1::]:
+        for currJob in self.listaDeTarefas[1::]:
 
             result = np.where(conjTarefas <= currJob.comeco)
 
@@ -24,12 +25,55 @@ class greed:
         return len(conjTarefas)
 
 
-    def intervalSchWei(self, ListaDeTarefas: list):
-        raise NotImplemented
+    def computeP(self, index) -> int:
+        esq = 0
+        dire = index - 1
+        testE = []
+        testD = []
+    
+
+        for i in self.listaDeTarefas:
+            testE.append(i.final)
+            testD.append(i.comeco)
+
+        
+        # print(f"{self.listaDeTarefas[index].nome} {index} - {esq + (dire - esq) // 2} {testD} {testE}")
+
+
+        while esq <= dire:
+            meio = np.right_shift(dire + esq, 1)
+
+            if self.listaDeTarefas[meio].final <= self.listaDeTarefas[index].comeco:
+                if self.listaDeTarefas[meio + 1].final <= self.listaDeTarefas[index].comeco:
+                    esq = meio + 1
+                else:
+                    return meio
+            else:
+                dire = meio - 1
+
+        return None
+    
+    def intervalSchWei(self):
+
+        for k in range(0, self.intervalParti()):
+            self.listaDeTarefas.sort(key=lambda x: x.final)
+            
+            self.jobs[k, 0] = self.listaDeTarefas[0]
+            self.jobs[k, -1] = self.listaDeTarefas[0].prioridade
+
+            for i in range(1, len(self.listaDeTarefas)):
+
+                tmpJob = self.listaDeTarefas[i]
+                self.jobs[k, -1] = self.listaDeTarefas[i].prioridade
+
+                P = self.computeP(i)
+                if P:
+                 self.jobs[k, -1] += self.jobs[k, P].prioridade
 
 
 
-g = greed([T.trabalho(0, 15, "Ola"), T.trabalho(2, 16, "A"),  T.trabalho(2, 17, "A"), T.trabalho(2, 18, "A"), T.trabalho(18, 20, "A"), T.trabalho(18, 20, "A"), T.trabalho(18, 20, "A"), T.trabalho(18, 20, "A"), T.trabalho(18, 20, "A")])
+
+g = greed([T.trabalho(0, 15, 15, "A"), T.trabalho(2, 16, 10,"B"),  T.trabalho(2, 17,10, "C"), T.trabalho(2, 18,10, "D"), T.trabalho(18, 20,10, "E"), T.trabalho(18, 20,10, "F"), T.trabalho(18, 20,27, "G"), T.trabalho(18, 20,10, "H"), T.trabalho(18, 20,10, "I")])
 
 
-print(g.intervalParti(g.listaDeTarefas))
+g.intervalSchWei()
--- utils/trabalho.py
+++ utils/trabalho.py
@@ -1,7 +1,7 @@
 # Classe para definir um novo trabalho a ser feito
 class trabalho:
 	
-	def __init__(self, comeco:float ,final:float, nome:str = None) -> None:
+	def __init__(self, comeco:float ,final:float, prioridade:float, nome:str = None) -> None:
 		"""
 		nome:str
 			Nome da tarefa que deve ser feita
@@ -10,5 +10,6 @@ class trabalho:
 		"""
 		self.nome = nome
 		self.comeco = comeco
+		self.prioridade = prioridade
 		self.final = final
 	
