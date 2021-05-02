d772f21b6785690916ee5e7692b6165675742538
Paulo Gonçalves Lima <paulogoncalves436@gmail.com>
2021-05-02 03:05:50 -0300

    Adicionando os comentarios,

--- utils/greed.py
+++ utils/greed.py
@@ -1,4 +1,5 @@
-from utils import trabalho as T
+
+import trabalho as T
 import numpy as np
 
 class pahtJobs:
@@ -12,11 +13,10 @@ class greed:
         self.listaDeTarefas = ListaDeTarefas
         self.final = []
 
-        # linhas, colunas = (self.intervalParti(), len(ListaDeTarefas) + 1)
-        # self.jobs = [[T.trabalho(0,0,0)] * colunas] * linhas
-        
-
     def intervalParti(self) -> int:
+        """
+        Função para poder retornar qual o minimo para poder fazer todas as tarefas.
+        """
         self.listaDeTarefas.sort(key=lambda x: x.comeco)
 
         conjTarefas = np.array([self.listaDeTarefas[0].final], dtype="float")
@@ -33,54 +33,55 @@ class greed:
         return len(conjTarefas)
 
     def computeP(self, index) -> int:
-        if index == 0:
-            return 0
-        for i in range(index, -1, -1):
+        """
+        Função para retornar o indice, da maior tarefa que é compativel com o indice inicial.
+        """
+        for i in range(index, -1, -1): # TODO: Busca Binaria, para poder achar.
             if self.listaDeTarefas[i].final <= self.listaDeTarefas[index].comeco:
                 return i
         return None
 
     def intervalSchWei(self):
-        totalPass = self.intervalParti()
-        ResultFinal = []
-        for k in range(0, totalPass):
+
+        for k in range(0, self.intervalParti()): # For para poder repetir X quantidades de vezes nas quais vão ser necessarias, para terminat todas as tarefas.
             self.listaDeTarefas.sort(key=lambda x: x.final)
-            caminhosPossiveis = []
+            caminhosPossiveis = [] 
 
-            for i in range(0, len(self.listaDeTarefas)):
+            for i in range(0, len(self.listaDeTarefas)): # Inicialização
                 caminhosPossiveis.append(pahtJobs())
-
+            
+            # Set da primeira escolha sendo o primeiro trabalho.
             caminhosPossiveis[0].total = self.listaDeTarefas[0].prioridade
             caminhosPossiveis[0].jobs.append(self.listaDeTarefas[0])
 
             for i in range(1, len(self.listaDeTarefas)):
 
-                tmpTotal = self.listaDeTarefas[i].prioridade
-                indexJob = self.computeP(i)
+                tmpTotal = self.listaDeTarefas[i].prioridade # A prioridade temporaria
+                indexJob = self.computeP(i) # Pegando o maior trabalho compativel, com o trabalho I.
 
-                if indexJob != None:
+                if indexJob != None: # Caso ache algum trabalho, adicione na prioridade temporaria.
                     tmpTotal += caminhosPossiveis[indexJob].total
 
-                if tmpTotal > caminhosPossiveis[i - 1].total:
+                if tmpTotal > caminhosPossiveis[i - 1].total: # Caso a nova prioridade seja maior que a escolha anterior.
                     caminhosPossiveis[i].total = tmpTotal
 
-                    if indexJob != None:
+                    if indexJob != None: 
                         caminhosPossiveis[i].jobs = caminhosPossiveis[indexJob].jobs
 
                     caminhosPossiveis[i].jobs.append(self.listaDeTarefas[i])
 
-                else:
+                else: # Se não, a nova escolha continua sendo a mesma que a anterior.
                     caminhosPossiveis[i] = caminhosPossiveis[i - 1]
 
-            for i, obj in enumerate(caminhosPossiveis[len(self.listaDeTarefas) - 1].jobs):
+            for i, obj in enumerate(caminhosPossiveis[len(self.listaDeTarefas) - 1].jobs): # Retirando as tarefas ja escolhidas.
                 self.listaDeTarefas.pop(self.listaDeTarefas.index(obj))
 
-            ResultFinal.append(caminhosPossiveis[len(self.listaDeTarefas) - 1].jobs)
+            self.final.append(caminhosPossiveis[len(self.listaDeTarefas) - 1].jobs)
 
-        return ResultFinal
+        return self.final # Return [[tarefa, tarefa], [tarefa], [tarefa, tarefa], [tarefa, tarefa]]
         
 
-# g = greed([T.trabalho(0, 15, 15, "A"), T.trabalho(2, 16, 10,"B"),  T.trabalho(2, 17,10, "C"), T.trabalho(2, 18,10, "D"), T.trabalho(18, 22, 10, "E"), T.trabalho(18, 22,10, "F"), T.trabalho(14, 20, 27, "G"), T.trabalho(18, 20,10, "H"), T.trabalho(18, 20,10, "I"), T.trabalho(20, 21,10, "J"),])
+g = greed([T.trabalho(0, 15, 15, "A"), T.trabalho(2, 16, 10,"B"),  T.trabalho(2, 17,10, "C"), T.trabalho(2, 18,10, "D"), T.trabalho(18, 22, 10, "E"), T.trabalho(18, 22,10, "F"), T.trabalho(14, 20, 27, "G"), T.trabalho(18, 20,10, "H"), T.trabalho(18, 20,10, "I"), T.trabalho(20, 21,10, "J"), T.trabalho(21, 22,10, "J"), T.trabalho(22, 23,10, "J"), T.trabalho(23, 24,10, "J"), T.trabalho(24, 25,10, "J")])
 
-
-# g.intervalSchWei()
+print(g.intervalParti())
+print(g.intervalSchWei())
